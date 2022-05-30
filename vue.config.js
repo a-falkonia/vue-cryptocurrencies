4c18a538045ff566b5fb3252f78d6b3a95ad7cc2
Anna <a.falkonia@gmail.com>
2022-05-30 10:51:33 +0400

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-cryptocurrencies/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-cryptocurrencies/public" : "/",
 });
